c360f4545bab02a800908c9f22505072f48b5cbf
Shane O'Moore <shane.omoore@livetilesglobal.com>
2021-10-16 16:07:36 +0100

    demo editing title of document

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Container, Header, Image, Grid, Placeholder, Button } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Container, Header, Image, Grid, Placeholder, Button, Input } from 'semantic-ui-react';
 import html2canvas from 'html2canvas';
 import './App.css';
 
 const capture = () => {
   html2canvas(document.querySelector(".resume")).then(canvas => {
     var a = document.createElement('a');
-    
+
     // a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
     a.href = canvas.toDataURL();
     a.download = 'resume.png';
@@ -15,10 +15,22 @@ const capture = () => {
 }
 
 function App() {
+  const [content, setContent] = useState({
+    header: "CV Builder",
+    isEditMode: false
+  });
+
+
   return (
     <>
       <Container className="resume">
-        <Header as='h1'>CV Builder</Header>
+        {!content.isEditMode ?
+          <Header as='h1' onClick={() => {
+            setContent({ ...content, isEditMode: true });
+          }}>{content.header}</Header>
+          :
+          <Input focus={true} defaultValue={content.header} onBlur={() => setContent({ ...content, isEditMode: false })} onChange={(_, input) => setContent({ ...content, header: input.value })} />
+        }
 
         <Image src="./avatar.jpg" circular size="small" />
 
