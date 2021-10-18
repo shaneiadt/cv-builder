cf7eee4ac72998451c065778d7766d957bc42124
Shane O'Moore <shaneiadt@gmail.com>
2021-10-18 09:39:10 +0100

    comment SectionToolbar for now

--- src/App.js
+++ src/App.js
@@ -47,35 +47,35 @@ const templates = [
   }
 ];
 
-const SectionToolbar = () => (
-  <div>
-    <Button.Group>
-      <Button icon>
-        <Icon name='align left' />
-      </Button>
-      <Button icon>
-        <Icon name='align center' />
-      </Button>
-      <Button icon>
-        <Icon name='align right' />
-      </Button>
-      <Button icon>
-        <Icon name='align justify' />
-      </Button>
-    </Button.Group>{' '}
-    <Button.Group>
-      <Button icon>
-        <Icon name='bold' />
-      </Button>
-      <Button icon>
-        <Icon name='underline' />
-      </Button>
-      <Button icon>
-        <Icon name='text width' />
-      </Button>
-    </Button.Group>
-  </div>
-)
+// const SectionToolbar = () => (
+//   <div>
+//     <Button.Group>
+//       <Button icon>
+//         <Icon name='align left' />
+//       </Button>
+//       <Button icon>
+//         <Icon name='align center' />
+//       </Button>
+//       <Button icon>
+//         <Icon name='align right' />
+//       </Button>
+//       <Button icon>
+//         <Icon name='align justify' />
+//       </Button>
+//     </Button.Group>{' '}
+//     <Button.Group>
+//       <Button icon>
+//         <Icon name='bold' />
+//       </Button>
+//       <Button icon>
+//         <Icon name='underline' />
+//       </Button>
+//       <Button icon>
+//         <Icon name='text width' />
+//       </Button>
+//     </Button.Group>
+//   </div>
+// )
 
 function App() {
   const [config, setConfig] = useState(templates[0]);
