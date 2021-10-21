fe24980f9f251f6f9478b4465d19ce448d25d2a1
Shane O'Moore <shaneiadt@gmail.com>
2021-10-21 16:40:54 +0100

    re-jig app component

--- src/App.js
+++ src/App.js
@@ -1,108 +1,21 @@
 import React, { useState } from 'react';
-import { Container, Header, Grid, Button, Icon, Image } from 'semantic-ui-react';
-import { lorem } from 'faker';
+import { Container, Grid, Button, Image } from 'semantic-ui-react';
 import { capture } from './utils';
-import { Renderer } from './Renderer';
-import { CustomEditor } from './Slate';
+import { Editor } from './Editor';
+import col1 from './templates/1';
+import col2 from './templates/2';
+
 import './App.css';
 
-const templates = [
-  {
-    id: 1,
-    title: "My Resume",
-    cols: [
-      [
-        {
-          "name": "editor1",
-          "component": CustomEditor,
-          "defaultValue": [
-            {
-              "type": 'heading-two',
-              "children": [{ text: 'Heading' }],
-            },
-            {
-              type: 'paragraph',
-              children: [{ text: lorem.sentences(2) }],
-            },
-          ]
-        },
-      ],
-      [
-        {
-          "name": "editor2",
-          "component": CustomEditor,
-          "defaultValue": [
-            {
-              "type": 'heading-two',
-              "children": [{ text: 'Heading' }],
-            },
-            {
-              type: 'paragraph',
-              children: [{ text: lorem.sentences(6) }],
-            },
-          ]
-        },
-      ]
-    ]
-  }
-];
-
-// const SectionToolbar = () => (
-//   <div>
-//     <Button.Group>
-//       <Button icon>
-//         <Icon name='align left' />
-//       </Button>
-//       <Button icon>
-//         <Icon name='align center' />
-//       </Button>
-//       <Button icon>
-//         <Icon name='align right' />
-//       </Button>
-//       <Button icon>
-//         <Icon name='align justify' />
-//       </Button>
-//     </Button.Group>{' '}
-//     <Button.Group>
-//       <Button icon>
-//         <Icon name='bold' />
-//       </Button>
-//       <Button icon>
-//         <Icon name='underline' />
-//       </Button>
-//       <Button icon>
-//         <Icon name='text width' />
-//       </Button>
-//     </Button.Group>
-//   </div>
-// )
+const template = {
+  cols: [col1, col2]
+};
 
 function App() {
-  const [config, setConfig] = useState(templates[0]);
-  const selectTemplate = (id) => setConfig({ ...templates.find(t => t.id === id) });
-  const [showEditButton, setShowEditButton] = useState(false);
-  const addItem = (colIndex) => {
-    const template = { ...config };
-
-    template.cols[colIndex].push(
-      {
-        "name": "editor2kasjhdkajsd",
-        "component": CustomEditor,
-        "defaultValue": [
-          {
-            "type": 'heading-two',
-            "children": [{ text: 'Heading' }],
-          },
-          {
-            type: 'paragraph',
-            children: [{ text: lorem.sentences(2) }],
-          },
-        ]
-      },
-    );
-
-    setConfig(template);
-  };
+  // const [config, setConfig] = useState(templates[0]);
+  // const selectTemplate = (id) => setConfig({ ...templates.find(t => t.id === id) });
+  // const [showEditButton, setShowEditButton] = useState(false);
+  const [avatar, setAvatar] = useState('https://via.placeholder.com/1000.png?text=Click+to+add+avatar');
 
   const onChangeAvatar = () => {
     const url = window.prompt("Enter new avatar URL");
@@ -111,69 +24,24 @@ function App() {
 
     // TODO: Valid image URL
     console.log({ url });
+
+    setAvatar(url);
   }
 
   return (
     <>
       <Container className="resume">
-        {config ?
-          <Grid>
-            <Grid.Row columns={2}>
-              <Grid.Column width={4}>
-                <Image style={{ cursor: 'pointer' }} src='https://via.placeholder.com/1000.png?text=Click+to+add+avatar' size='medium' circular onClick={onChangeAvatar} />
-              </Grid.Column>
-              <Grid.Column width={12}>
-                <Renderer config={[{
-                  "name": "h1", "component": CustomEditor, "defaultValue": [{ type: "heading-one", children: [{ text: "My Resume" }] }, {
-                    type: 'paragraph',
-                    children: [{ text: lorem.sentences(4) }],
-                  },]
-                }]} />
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns={2} divided>
-              <Grid.Column width={4}>
-                <Renderer config={config.cols[0]} />
-                <div style={{ padding: '20px 0' }} onMouseEnter={() => setShowEditButton(true)} onMouseLeave={() => setShowEditButton(false)}>
-                  {showEditButton &&
-                    <Button fluid icon onClick={() => addItem(0)}>
-                      <Icon name='plus' />
-                    </Button>
-                  }
-                </div>
-              </Grid.Column>
-              <Grid.Column width={12}>
-                <Renderer config={config.cols[1]} />
-                <div style={{ padding: '20px 0' }} onMouseEnter={() => setShowEditButton(true)} onMouseLeave={() => setShowEditButton(false)}>
-                  {showEditButton &&
-                    <Button fluid icon onClick={() => addItem(1)}>
-                      <Icon name='plus' />
-                    </Button>
-                  }
-                </div>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-          :
-          <Grid>
-            <Grid.Row>
-              <Grid.Column textAlign="center">
-                <Header as="h1">Select a template to use</Header>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns={3}>
-              <Grid.Column textAlign="center">
-                <Button onClick={() => selectTemplate(1)}>Template 1</Button>
-              </Grid.Column>
-              <Grid.Column textAlign="center">
-                <Button onClick={() => selectTemplate(1)}>Template 2</Button>
-              </Grid.Column>
-              <Grid.Column textAlign="center">
-                <Button onClick={() => selectTemplate(1)}>Template 3</Button>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        }
+        <Grid divided>
+          <Grid.Row columns={2}>
+            <Grid.Column width={4}>
+              <Image style={{ cursor: 'pointer' }} src={avatar} size='medium' circular onClick={onChangeAvatar} />
+              <Editor template={template.cols[0]} />
+            </Grid.Column>
+            <Grid.Column width={12}>
+              <Editor template={template.cols[1]} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
 
       <Container>
