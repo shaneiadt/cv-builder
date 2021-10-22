54d65e17638807822ee4d34a6bd33abe76bab532
Shane O'Moore <shaneiadt@gmail.com>
2021-10-22 12:32:15 +0100

    no indenting

--- src/Editor.js
+++ src/Editor.js
@@ -33,8 +33,7 @@ const modules = {
         [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
         [{ size: [] }],
         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ 'list': 'ordered' }, { 'list': 'bullet' },
-        { 'indent': '-1' }, { 'indent': '+1' }],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
         ['link', 'image', 'video'],
         ['clean']
     ],
