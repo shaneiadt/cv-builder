eeb339ddc4699c5926d35fe4146cad21d31f72be
Shane O'Moore <shaneiadt@gmail.com>
2021-10-22 12:32:42 +0100

    render download link

--- src/PDF.js
+++ src/PDF.js
@@ -1,40 +1,18 @@
 import React from 'react';
-import { Page, Text, View, Document, StyleSheet, PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
-
-// Create styles
-const styles = StyleSheet.create({
-    page: {
-        flexDirection: 'row',
-        backgroundColor: '#E4E4E4'
-    },
-    section: {
-        margin: 10,
-        padding: 10,
-        flexGrow: 1
-    }
-});
-
-// Create Document Component
-const MyDocument = () => (
-    <Document>
-        <Page size="A4" style={styles.page}>
-            <View style={styles.section}>
-                <Text>Section #1</Text>
-            </View>
-            <View style={styles.section}>
-                <Text>Section #2</Text>
-            </View>
-        </Page>
-    </Document>
-);
+// import { PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import { Quixote } from './Resume';
 
 export const PDF = () => (
     <>
-        <PDFViewer>
-            <MyDocument />
+        {/* <PDFViewer width="1000" height="1000">
+            <Quixote />
         </PDFViewer>
-        <PDFDownloadLink document={<MyDocument />} fileName={"FileName"}>
-            <button> Download </button>
-        </PDFDownloadLink>
+        <br/> */}
+        <div style={{ textAlign: 'center' }}>
+            <PDFDownloadLink document={<Quixote />} fileName={"Resume"}>
+                <button style={{ width: '200px', padding: '10px' }}> Download </button>
+            </PDFDownloadLink>
+        </div>
     </>
 );
