af5559b37d0e676824e23168668a0251b4baa9a7
Shane O'Moore <shaneiadt@gmail.com>
2021-10-23 10:56:44 +0100

    clean up app component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { useState } from 'react';
-import { Image } from 'semantic-ui-react';
-// import { capture } from './utils';
-import { Editor } from './Editor';
-import col1 from './templates/1';
-import col2 from './templates/2';
+import { Container, Grid, Image } from 'semantic-ui-react';
+import { capture } from './utils';
+import { PDF } from './PDF';
+import Editable from './Editable/Editable';
 
 import './App.css';
-import { PDF } from './PDF';
-
-const template = {
-  cols: [col1, col2]
-};
 
 function App() {
-  // const [config, setConfig] = useState(templates[0]);
-  // const selectTemplate = (id) => setConfig({ ...templates.find(t => t.id === id) });
-  // const [showEditButton, setShowEditButton] = useState(false);
   const [avatar, setAvatar] = useState('https://via.placeholder.com/1000.png?text=Click+to+add+avatar');
 
   const onChangeAvatar = () => {
@@ -31,24 +22,23 @@ function App() {
 
   return (
     <>
-      <div className="container">
-        <div className="resume">
-          <div>
-            <Image style={{ cursor: 'pointer' }} src={avatar} size='medium' circular onClick={onChangeAvatar} />
-            <Editor template={template.cols[0]} />
-          </div>
-          <div>
-            <Editor template={template.cols[1]} />
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Grid className="resume" divided>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              <Image style={{ cursor: 'pointer' }} src={avatar} size='small' circular onClick={onChangeAvatar} />
+            </Grid.Column>
+            <Grid.Column width={12}>
+              <Editable />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
 
-      {/* <div className="toolbar">
-        <div>
-          <button onClick={() => capture(".resume")}>Download</button>
-        </div>
-      </div> */}
-      <PDF />
+      <Container className="toolbar">
+        <button onClick={() => capture(".resume")}>Download Image</button>
+        <PDF />
+      </Container>
     </>
   );
 }
