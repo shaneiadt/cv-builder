95dc9ab0658e62d2484908039cd2f3b663474cd9
Shane O'Moore <shaneiadt@gmail.com>
2021-10-23 14:21:28 +0100

    update app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { Container, Grid, Image } from 'semantic-ui-react';
-import { capture } from './utils';
+// import { capture } from './utils';
 import { PDF } from './PDF';
-import Editable from './Editable/Editable';
 
 import './App.css';
+import Editable from './Editable/Editable';
+import { Popup } from './Popup/Popup';
 
 function App() {
   const [avatar, setAvatar] = useState('https://via.placeholder.com/1000.png?text=Click+to+add+avatar');
+  const [message, setMessage] = useState("");
 
   const onChangeAvatar = () => {
     const url = window.prompt("Enter new avatar URL");
@@ -20,23 +22,27 @@ function App() {
     setAvatar(url);
   }
 
+  const onPopupComplete = () => setMessage("");
+
   return (
     <>
+      <Popup message={message} onPopupComplete={onPopupComplete} />
       <Container>
-        <Grid className="resume" divided>
+        <Grid className="resume" divided columns={2} padded='horizontally'>
           <Grid.Row>
             <Grid.Column width={4}>
-              <Image style={{ cursor: 'pointer' }} src={avatar} size='small' circular onClick={onChangeAvatar} />
+              <Image style={{ cursor: 'pointer' }} src={avatar} size='medium' circular onClick={onChangeAvatar} />
             </Grid.Column>
             <Grid.Column width={12}>
-              <Editable />
+              <Editable html="<h2>The title</h2>" />
+              <Editable html="<p>The title</p>" />
             </Grid.Column>
           </Grid.Row>
         </Grid>
       </Container>
 
       <Container className="toolbar">
-        <button onClick={() => capture(".resume")}>Download Image</button>
+        {/* <button onClick={() => capture(".resume")}>Download Image</button> */}
         <PDF />
       </Container>
     </>
