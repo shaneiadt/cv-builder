78eb3b9e426ea2d4c439ddd71d3ead3e8f4f0306
Shane O'Moore <shaneiadt@gmail.com>
2021-10-30 13:19:12 +0100

    simplify toolbar

--- src/Toolbar/Toolbar.js
+++ src/Toolbar/Toolbar.js
@@ -1,25 +1,14 @@
-import React, { useState } from "react";
-import { ELEMENT_TYPES } from '../utils/Types';
-import { Button, Icon } from "semantic-ui-react";
+import React from "react";
+import { Button } from "semantic-ui-react";
 
 import './Toolbar.css';
 
 export const Toolbar = ({ addEditable }) => {
-    const [visible, setVisible] = useState(false);
-
     return (
-        <div style={{ marginTop: '20px' }}>
-            <div className="toolbar" onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>
-                {!visible ?
-                    <Icon name="plus" style={{ fontSize: '12px', color: 'grey' }} />
-                    :
-                    <>
-                        <Button onClick={() => addEditable("<h1>Heading 1</h1>", ELEMENT_TYPES.H1)}>+ Heading 1</Button>
-                        <Button onClick={() => addEditable("<h2>Heading 2</h2>", ELEMENT_TYPES.H2)}>+ Heading 2</Button>
-                        <Button onClick={() => addEditable("<p>write something...</p>", ELEMENT_TYPES.BODY)}> +Paragraph</Button>
-                    </>
-                }
-            </div>
+        <div className="toolbar">
+            <Button onClick={() => addEditable("<h1>Heading 1</h1>")}>+ Heading 1</Button>
+            <Button onClick={() => addEditable("<h2>Heading 2</h2>")}>+ Heading 2</Button>
+            <Button onClick={() => addEditable("<p>write something...</p>")}> +Paragraph</Button>
         </div>
     );
 }
