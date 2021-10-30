4fe8383ae45922385eba93d73a5cd45196a4a5b3
Shane O'Moore <shaneiadt@gmail.com>
2021-10-30 13:19:37 +0100

    hide stuff for nw

--- src/PDF.js
+++ src/PDF.js
@@ -3,16 +3,19 @@ import React from 'react';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import { Resume } from './Resume';
 
-export const PDF = ({ content }) => (
-    <>
-        {/* <PDFViewer width="1000" height="1000">
+export const PDF = ({ content }) => {
+    console.log({ content });
+    return (
+        <>
+            {/* <PDFViewer width="1000" height="1000">
             <Quixote />
         </PDFViewer>
         <br/> */}
-        <div style={{ textAlign: 'center' }}>
-            <PDFDownloadLink document={<Resume content={content} />} fileName={"Resume"}>
+            <div style={{ textAlign: 'center' }}>
+                {/* <PDFDownloadLink document={<Resume content={content} />} fileName={"Resume"}>
                 <button style={{ width: '200px', padding: '10px' }}>Download PDF</button>
-            </PDFDownloadLink>
-        </div>
-    </>
-);
+            </PDFDownloadLink> */}
+            </div>
+        </>
+    );
+}
