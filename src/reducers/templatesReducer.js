6c3faaa8419cb419d0513f2af8066c681e795794
Shane O'Moore <shaneiadt@gmail.com>
2021-11-30 13:14:27 +0000

    update template reducer

--- src/reducers/templatesReducer.js
+++ src/reducers/templatesReducer.js
@@ -6,6 +6,16 @@ const templatesReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case ACTIONS.LOAD_TEMPLATE:
             return { ...action.payload }
+        case ACTIONS.UPDATE_BLOCK:
+            const { columnIndex, blockIndex, block } = action.payload;
+            const newState = { ...state };
+
+            newState.layout.cols[columnIndex].blocks[blockIndex] = {
+                ...newState.layout.cols[columnIndex].blocks[blockIndex],
+                ...block
+            };
+
+            return newState;
         default:
             return state;
     }
