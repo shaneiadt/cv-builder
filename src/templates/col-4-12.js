7cdc6a784b52c208c4273cf5df3f7c8f33bd877f
Shane O'Moore <shaneiadt@gmail.com>
2021-12-19 13:51:32 +0000

    add id's to block elements

--- src/templates/col-4-12.js
+++ src/templates/col-4-12.js
@@ -8,13 +8,16 @@ const template = {
                 width: 4,
                 blocks: [
                     {
+                        id: 1,
                         type: BLOCK_TYPE.AVATAR,
                         image: './avatar.jpg'
                     },
                     {
+                        id: 2,
                         type: BLOCK_TYPE.DEFAULT,
                         header: "Header",
                         text: "Aliquam erat volutpat. Aliquam rhoncus ligula sed nisl suscipit faucibus. Phasellus eleifend lorem sed cursus accumsan. Suspendisse condimentum nisi erat, suscipit mollis nisl fringilla at. Maecenas gravida libero non porta laoreet.",
+                        labels: []
                     }
                 ]
             },
@@ -22,15 +25,17 @@ const template = {
                 width: 12,
                 blocks: [
                     {
+                        id: 3,
                         type: BLOCK_TYPE.DEFAULT,
-                        header: "Header",
+                        header: "Header 1",
                         subheader: "Subheader",
                         text: "Aliquam erat volutpat. Aliquam rhoncus ligula sed nisl suscipit faucibus. Phasellus eleifend lorem sed cursus accumsan. Suspendisse condimentum nisi erat, suscipit mollis nisl fringilla at. Maecenas gravida libero non porta laoreet.",
                         labels: ["ReactJs"]
                     },
                     {
+                        id: 4,
                         type: BLOCK_TYPE.DEFAULT,
-                        header: "Header",
+                        header: "Header 2",
                         subheader: "Subheader",
                         text: "Aliquam erat volutpat. Aliquam rhoncus ligula sed nisl suscipit faucibus. Phasellus eleifend lorem sed cursus accumsan. Suspendisse condimentum nisi erat, suscipit mollis nisl fringilla at. Maecenas gravida libero non porta laoreet.",
                         labels: ["HTML", "CSS"]
