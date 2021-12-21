0f280aab9247cc6bff6360c95850281bf44c7faa
Shane O'Moore <shaneiadt@gmail.com>
2021-12-21 15:01:12 +0000

    update toolbar component

--- src/Toolbar/Toolbar.js
+++ src/Toolbar/Toolbar.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
+import { connect } from "react-redux";
 
 import './Toolbar.css';
+import { addBlock } from "../actions";
 
-export const Toolbar = ({ addEditable }) => {
+const Toolbar = ({ addBlock, columnIndex }) => {
     return (
         <div className="toolbar">
-            <Button onClick={() => addEditable("<h1>Heading 1</h1>")}>+ Heading 1</Button>
-            <Button onClick={() => addEditable("<h2>Heading 2</h2>")}>+ Heading 2</Button>
-            <Button onClick={() => addEditable("<p>write something...</p>")}> +Paragraph</Button>
+            <Button onClick={() => addBlock(columnIndex)}>+ Add Block</Button>
         </div>
     );
 }
+
+export default connect(null, { addBlock })(Toolbar);
